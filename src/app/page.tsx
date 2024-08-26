8243a2a37d1f7a1bc9fd406ed96fa0223956aaa4
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-26 12:12:34 +0100

    new chat button with new component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,21 +14,7 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="lg:mb-10 sm:mb-2 text-center content-center">
-        <button
-          className="group rounded-lg px-5 py-4 transition-colors content-center hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 border border-transparent wx-64"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            New chat{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 text-sm opacity-50">
-            Start a new chat.
-          </p>
-        </button>
-      </div>
+      <MainMenuItem title="New chat" subtitle="Start a new chat" />
 
       <MainMenuItem title="Options" subtitle="Configure application from here."/>
     </main>
