bbe6146277e722921d8d24ac52150e534ad3ae90
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-26 11:56:52 +0100

    add to main menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import MainMenuItem from "@/components/ui/main-menu-item";
+
 export default function Home() {
   return (
-    <main className="flex-col items-center justify-between p-24 content-center">
+    <main className="flex-col items-center justify-start lg:p-24 md:p-10 content-center">
       <section>
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
@@ -12,7 +14,7 @@ export default function Home() {
         </div>
       </section>
 
-      <div className="mb-32 text-center content-center">
+      <div className="lg:mb-10 sm:mb-2 text-center content-center">
         <button
           className="group rounded-lg px-5 py-4 transition-colors content-center hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 border border-transparent wx-64"
         >
@@ -27,6 +29,8 @@ export default function Home() {
           </p>
         </button>
       </div>
+
+      <MainMenuItem title="Options" subtitle="Configure application from here."/>
     </main>
   );
 }
