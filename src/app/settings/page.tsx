2200fbda89846604378936f8da7d49d46f97a4a6
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-27 14:05:06 +0100

    get settings function

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,27 +1,66 @@
-"use client"
-import { WebviewWindow } from "@tauri-apps/api/window"
-import { useEffect, useState } from "react"
+"use client";
+import { WebviewWindow } from "@tauri-apps/api/window";
+import { useEffect, useState } from "react";
+import {
+  exists,
+  BaseDirectory,
+  readTextFile,
+  writeTextFile,
+} from "@tauri-apps/api/fs";
 
+type AppSettings = {
+  apiKey?: string;
+};
+
+async function getSettings(): Promise<AppSettings> {
+  if (
+    await exists("chat-with-gpt-settings.json", {
+      dir: BaseDirectory.AppConfig,
+    })
+  ) {
+    let jsonString = await readTextFile("chat-with-gpt-settings.json", {
+      dir: BaseDirectory.AppConfig,
+    });
+    let settings: AppSettings = JSON.parse(jsonString);
+    return settings;
+  }
+  return { apiKey: undefined };
+}
 
 export default function SettingsPage() {
-    // Set up app window
-    const [appWindow, setAppWindow] = useState<WebviewWindow>()
-    async function setupAppWindow() {
-        const appWindow = (await import("@tauri-apps/api/window")).appWindow
-        setAppWindow(appWindow)
-    }
-    useEffect(() => {
-        setupAppWindow()
-    }, [])
-    
-    return (
-        <div className="sm:p-5 lg:p-20 space-y-2 flex flex-col">
-            <h1 className="text-3xl font-semibold pb-4">Settings</h1>
-            <div className="inline space-x-2">
-                <label className="text-semibold" htmlFor="api-key">API key:&nbsp;</label>
-                <input id="api-key" type="text" placeholder="API key here..." className="bg-transparent border-2 dark:border-white rounded-md"/>
-                <button className="rounded-md text-blue-200 border-blue-200 border-2 min-w-20">Save</button>
-            </div>
-        </div>
-    )
+  // Set up app window
+  const [appWindow, setAppWindow] = useState<WebviewWindow>();
+  const [appSettings, setAppSettings] = useState<AppSettings>({
+    apiKey: undefined,
+  });
+  async function setupAppWindow() {
+    const appWindow = (await import("@tauri-apps/api/window")).appWindow;
+    setAppWindow(appWindow);
+  }
+  useEffect(() => {
+    setupAppWindow();
+    getSettings().then((settings) => setAppSettings(settings));
+  }, []);
+
+  return (
+    <div className="sm:p-5 lg:p-20 space-y-2 flex flex-col">
+      <h1 className="text-3xl font-semibold pb-4">Settings</h1>
+      <div className="inline space-x-2">
+        <label className="text-semibold" htmlFor="api-key">
+          API key:&nbsp;
+        </label>
+        <input
+          id="api-key"
+          type="text"
+          placeholder={
+            appSettings.apiKey ? appSettings.apiKey : "API key here..."
+          }
+          className="bg-transparent border-2 dark:border-white rounded-md"
+        />
+        <button className="rounded-md text-blue-200 border-blue-200 border-2 min-w-20">
+          Save
+        </button>
+      </div>
+    </div>
+  );
 }
