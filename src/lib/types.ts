fed50ce7687cf8ef2699b00dada76b3e14fb604a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-28 16:16:28 +0100

    remove unneeded types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,30 +2,3 @@ export type AppSettings = {
     apiKey?: string;
     model?: string;
 };
-
-export type ChatGPTMessage = {
-    role: string;
-    message: string;
-}
-
-export type Choice = {
-    finish_reason: string;
-    index: number;
-    message: ChatGPTMessage;
-    logprobs?: any;
-}
-
-export type Usage = {
-    completion_tokens: number;
-    prompt_tokens: number;
-    total_tokens: number;
-}
-
-export type ChatGPTResponse = {
-    choices: Array<Choice>;
-    created: number;
-    id: string;
-    model: string;
-    object: string;
-    usage: Usage;
-}
