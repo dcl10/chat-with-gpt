9ccb55609851096709062e60521361a8e3f0a456
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-08-29 13:12:23 +0100

    rendering of messages from state

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -2,13 +2,27 @@
 
 import ChatInput from "@/components/ui/chat-input";
 import BackButton from "@/components/ui/back-button";
+import ChatBubble from "@/components/ui/chat-bubble";
+import { TextAndRole } from "@/lib/types";
+import { useState } from "react";
 
 export default function ChatPage() {
+  const [chatHistory, setChatHistory] = useState<TextAndRole[]>([]);
+
+  function addToHistory(text: string, isUser: boolean) {
+    setChatHistory([...chatHistory, { text, isUser }]);
+  }
+
   return (
-    <div className="sm:p-5 lg:p-20 space-y-2 flex flex-col">
+    <div className="sm:p-5 lg:p-20 space-y-4 justify-center">
       <BackButton onClick={() => (window.location.href = "/")} />
-      <div className="flex flex-col items-center justify-center fixed bottom-4 inset-x-2">
-        <ChatInput onClick={() => console.log("clicked")} />
+      {chatHistory.map((textAndRole: TextAndRole) => (
+        <ChatBubble text={textAndRole.text} isUser={textAndRole.isUser} />
+      ))}
+      <div className="flex items-center justify-center fixed bottom-4 inset-x-2">
+        <ChatInput
+          onClick={addToHistory}
+        />
       </div>
     </div>
   );
