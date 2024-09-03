e427e87ca4bd2fcca7f8fa65d7202c384a9fc281
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-03 14:49:19 +0100

    response structs

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -7,3 +7,25 @@ pub struct ChatGptRequest {
     pub model: String,
     pub messages: Vec<Message>
 }
+
+pub struct Choice {
+    pub finish_reason: String,
+    pub index: usize,
+    pub message: Message,
+    pub logprobs: isize
+}
+
+pub struct Usage {
+    pub completion_tokens: usize,
+    pub prompt_tokens: usize,
+    pub total_tokens: usize
+}
+
+pub struct ChatGptResponse {
+    pub choices: Vec<Choice>,
+    pub created: usize,
+    pub id: String,
+    pub model: String,
+    pub object: String,
+    pub usage: Usage
+}
