02871c2c8e0a2147b98378da35c8c0faa91252eb
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-04 13:03:52 +0100

    chat page navigation

--- src/components/ui/back-button.tsx
+++ src/components/ui/back-button.tsx
@@ -3,7 +3,10 @@ import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 export default function BackButton({ onClick }: { onClick: any }) {
   return (
-    <button className="content-start items-center space-x-2 inline-flex max-w-20" onClick={onClick}>
+    <button
+      className="content-start items-center space-x-2 inline-flex max-w-20"
+      onClick={onClick}
+    >
       <ArrowLeftIcon className="size-8 text-blue-200 hover:text-white rounded-full border-2 border-blue-200 hover:border-white" />
       <p className="test-semibold text-white">Back</p>
     </button>
