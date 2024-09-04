46151838376e1e728027ccdc2da707cd81ab8870
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-04 12:20:00 +0100

    chat gpt response types in TS

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,33 @@
+import { ChatRole } from "./enums";
+
 export type AppSettings = {
     apiKey: string;
     model: string;
 };
 
-export type TextAndRole = {
-    text: string;
-    isUser: boolean;
+export type Choice = {
+    finish_reason: string;
+    index: number;
+    message: Message;
+    logprobs: number | null;
+}
+
+export type Message = {
+    role: ChatRole;
+    content: string;
+}
+
+export type Usage = {
+    completion_tokens: number;
+    prompt_tokens: number;
+    total_tokens: number;
+}
+
+export type ChatGptResponse = {
+    choices: Choice[];
+    created: number;
+    id: string;
+    model: string;
+    object: string;
+    usage: Usage;
 }
