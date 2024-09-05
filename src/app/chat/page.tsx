1d51168152bd54623eb3b6a7211eeb5221be840a
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-05 19:18:23 +0100

    use configured model (#18)

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,26 +3,27 @@
 import ChatInput from "@/components/ui/chat-input";
 import TitleBar from "@/components/ui/title-bar";
 import ChatBubble from "@/components/ui/chat-bubble";
-import { ChatGptResponse, Message } from "@/lib/types";
-import { useState } from "react";
+import { AppSettings, ChatGptResponse, Message } from "@/lib/types";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { ChatRole } from "@/lib/enums";
-import { useRouter } from "next/navigation";
 
 export default function ChatPage() {
   const [chatHistory, setChatHistory] = useState<Message[]>([]);
-  const router = useRouter();
+  const [appSettings, setAppSettings] = useState<AppSettings>();
 
-  function goBack() {
-    router.back();
-  }
+  useEffect(() => {
+    invoke<AppSettings>("get_settings").then((settings) =>
+      setAppSettings(settings)
+    );
+  }, []);
 
   async function addToHistory(message: Message) {
     setChatHistory((prev) => [...prev, message]);
     try {
       let response: ChatGptResponse = await invoke("chat_to_model", {
         request: {
-          model: "gpt-4o-mini",
+          model: appSettings?.model,
           messages: [{ role: message.role, content: message.content }],
         },
       });
@@ -36,7 +37,7 @@ export default function ChatPage() {
 
   return (
     <div className="relative">
-      <TitleBar title="Chat"/>
+      <TitleBar title="Chat" />
       <div className="pt-20"></div>
       <div className="sm:p-5 lg:p-20 space-y-4 justify-center">
         {chatHistory.map((message: Message, key) => (
