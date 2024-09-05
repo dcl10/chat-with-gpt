96677cb748a8d259646cf683e51de61ae206306e
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-05 18:51:43 +0100

    show on page

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import ChatInput from "@/components/ui/chat-input";
-import BackButton from "@/components/ui/back-button";
+import TitleBar from "@/components/ui/title-bar";
 import ChatBubble from "@/components/ui/chat-bubble";
 import { ChatGptResponse, Message } from "@/lib/types";
 import { useState } from "react";
@@ -14,11 +14,11 @@ export default function ChatPage() {
   const router = useRouter();
 
   function goBack() {
-    router.back()
+    router.back();
   }
 
   async function addToHistory(message: Message) {
-    setChatHistory(prev => [...prev, message])
+    setChatHistory((prev) => [...prev, message]);
     try {
       let response: ChatGptResponse = await invoke("chat_to_model", {
         request: {
@@ -28,24 +28,28 @@ export default function ChatPage() {
       });
       let messages: Message[] = [];
       response.choices.map((choice) => messages.push(choice.message));
-      setChatHistory(prev => [...prev, ...messages]);
+      setChatHistory((prev) => [...prev, ...messages]);
     } catch (error) {
       console.error(error);
     }
   }
 
   return (
-    <div className="sm:p-5 lg:p-20 space-y-4 justify-center">
-      <BackButton onClick={goBack} />
-      {chatHistory.map((message: Message, key) => (
-        <ChatBubble
-          key={key}
-          text={message.content}
-          isUser={message.role === ChatRole.User}
-        />
-      ))}
-      <div className="flex items-center justify-center fixed bottom-4 inset-x-2">
-        <ChatInput onClick={addToHistory} />
+    <div className="relative">
+      <TitleBar title="Chat"/>
+      <div className="pt-20"></div>
+      <div className="sm:p-5 lg:p-20 space-y-4 justify-center">
+        {chatHistory.map((message: Message, key) => (
+          <ChatBubble
+            key={key}
+            text={message.content}
+            isUser={message.role === ChatRole.User}
+          />
+        ))}
+        <div className="pb-10"></div>
+        <div className="flex items-center justify-center fixed bottom-4 inset-x-2">
+          <ChatInput onClick={addToHistory} />
+        </div>
       </div>
     </div>
   );
