ba338632a58d8c166eba0a4480dff54826fd712c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-05 17:05:18 +0100

    Bugfix/remove unused chatgpt.ts (#14)
    * remove imports
    
    * delete chatgpt.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import {
-  exists,
-  BaseDirectory,
-  readTextFile,
-} from "@tauri-apps/api/fs";
-import { AppSettings } from "@/lib/types";
 
 function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
