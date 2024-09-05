065220f7f9cd1ab88e20b39f7b5bd2512dee592c
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-05 12:11:39 +0100

    Restyle main page (#12)
    * fix jumbotron title colour
    
    * format main page
    
    * fix background
    
    * restyle main page links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,18 +17,35 @@ export default function Home() {
     <main className="flex-col items-center justify-start lg:p-24 md:p-10 content-center">
       <section>
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl dark:text-white">
             Welcome to Chat with GPT
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-            A desktop version of ChatGPT using <a href="https://tauri.app/" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-green-600">Tauri</a>
+            A desktop version of ChatGPT using{" "}
+            <a
+              href="https://tauri.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-700 hover:decoration-solid hover:underline"
+            >
+              Tauri
+            </a>
           </p>
         </div>
       </section>
 
-      <MainMenuItem title="New chat" subtitle="Start a new chat" onClick={goToChat}/>
-
-      <MainMenuItem title="Settings" subtitle="Configure application from here." onClick={goToSettings}/>
+      <div className="space-y-2">
+        <MainMenuItem
+          title="New chat"
+          subtitle="Start a new chat"
+          onClick={goToChat}
+        />
+        <MainMenuItem
+          title="Settings"
+          subtitle="Configure application from here."
+          onClick={goToSettings}
+        />
+      </div>
     </main>
   );
 }
