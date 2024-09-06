922856107577f1eee3357de208c7966005a5693f
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-06 11:26:14 +0100

    validation

--- src/components/ui/chat-input.tsx
+++ src/components/ui/chat-input.tsx
@@ -5,6 +5,11 @@ import { ChatRole } from "@/lib/enums";
 
 export default function ChatInput({ onClick }: { onClick: Function }) {
   const [text, setText] = useState("");
+
+  function validate(input: string): boolean {
+    return input.length > 0;
+  }
+
   return (
     <div className="relative w-full max-w-md">
       <TextInput
@@ -13,10 +18,11 @@ export default function ChatInput({ onClick }: { onClick: Function }) {
         placeholder="Type a message..."
         className="rounded-full"
         sizing={"md"}
+        color={"blue"}
         value={text}
         onChange={(event) => setText(event.target.value)}
         onKeyDown={(event) => {
-          if (event.code === "Enter") {
+          if (event.code === "Enter" && validate(text)) {
             onClick({ content: text, role: ChatRole.User });
             setText("");
           }
@@ -25,11 +31,14 @@ export default function ChatInput({ onClick }: { onClick: Function }) {
       <Button
         type="button"
         onClick={() => {
-          onClick({ content: text, role: ChatRole.User });
-          setText("");
+          if (validate(text)) {
+            onClick({ content: text, role: ChatRole.User });
+            setText("");
+          }
         }}
         className="items-center absolute right-1 top-1/2 transform -translate-y-1/2 rounded-full bg-neutral-200 text-black hover:text-white"
         color={"dark"}
+        disabled={!validate(text)}
       >
         <ArrowUpIcon className="size-4 rounded-full" />
       </Button>
