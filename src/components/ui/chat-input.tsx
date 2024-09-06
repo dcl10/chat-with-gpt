31b67bcfc7db5cf4950a5dcbcc8fb3b460dea990
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-06 11:45:12 +0100

    button spinner while processing

--- src/components/ui/chat-input.tsx
+++ src/components/ui/chat-input.tsx
@@ -5,6 +5,7 @@ import { ChatRole } from "@/lib/enums";
 
 export default function ChatInput({ onClick }: { onClick: Function }) {
   const [text, setText] = useState("");
+  const [isProcessing, setIsProcessing] = useState(false);
 
   function validate(input: string): boolean {
     return input.length > 0;
@@ -21,24 +22,29 @@ export default function ChatInput({ onClick }: { onClick: Function }) {
         color={"gray"}
         value={text}
         onChange={(event) => setText(event.target.value)}
-        onKeyDown={(event) => {
+        onKeyDown={async (event) => {
           if (event.code === "Enter" && validate(text)) {
-            onClick({ content: text, role: ChatRole.User });
             setText("");
+            setIsProcessing((prev) => !prev);
+            await onClick({ content: text, role: ChatRole.User });
+            setIsProcessing((prev) => !prev);
           }
         }}
       />
       <Button
         type="button"
-        onClick={() => {
+        onClick={async () => {
           if (validate(text)) {
-            onClick({ content: text, role: ChatRole.User });
             setText("");
+            setIsProcessing((prev) => !prev);
+            await onClick({ content: text, role: ChatRole.User });
+            setIsProcessing((prev) => !prev);
           }
         }}
         className="items-center absolute right-1 top-1/2 transform -translate-y-1/2 rounded-full text-white"
         color={"blue"}
         disabled={!validate(text)}
+        isProcessing={isProcessing}
       >
         <ArrowUpIcon className="size-4 rounded-full" />
       </Button>
