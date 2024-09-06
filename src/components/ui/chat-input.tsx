4fd31a08c121cc4bc0f93d3613d81040e19e9476
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-06 11:10:16 +0100

    allow enter key to send message

--- src/components/ui/chat-input.tsx
+++ src/components/ui/chat-input.tsx
@@ -1,28 +1,38 @@
-import { Button, TextInput } from 'flowbite-react';
+import { Button, TextInput } from "flowbite-react";
 import { ArrowUpIcon } from "@heroicons/react/24/outline";
-import { useRef } from 'react';
-import { ChatRole } from '@/lib/enums';
+import { useState } from "react";
+import { ChatRole } from "@/lib/enums";
 
-export default function ChatInput({onClick}:{onClick: Function}) {
-    const ref = useRef("")
-    return (
-        <div className="relative w-full max-w-md">
-          <TextInput
-            id="chat-input"
-            type="text"
-            placeholder="Type a message..."
-            className="rounded-full"
-            sizing={"md"}
-            onChange={(event) => ref.current = event.target.value}
-          />
-          <Button
-            type="button"
-            onClick={() => onClick({content: ref.current, role: ChatRole.User})}
-            className="items-center absolute right-1 top-1/2 transform -translate-y-1/2 rounded-full bg-neutral-200 text-black hover:text-white"
-            color={"dark"}
-          >
-            <ArrowUpIcon className="size-4 rounded-full" />
-          </Button>
-        </div>
-      );
+export default function ChatInput({ onClick }: { onClick: Function }) {
+  const [text, setText] = useState("");
+  return (
+    <div className="relative w-full max-w-md">
+      <TextInput
+        id="chat-input"
+        type="text"
+        placeholder="Type a message..."
+        className="rounded-full"
+        sizing={"md"}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        onKeyDown={(event) => {
+          if (event.code === "Enter") {
+            onClick({ content: text, role: ChatRole.User });
+            setText("");
+          }
+        }}
+      />
+      <Button
+        type="button"
+        onClick={() => {
+          onClick({ content: text, role: ChatRole.User });
+          setText("");
+        }}
+        className="items-center absolute right-1 top-1/2 transform -translate-y-1/2 rounded-full bg-neutral-200 text-black hover:text-white"
+        color={"dark"}
+      >
+        <ArrowUpIcon className="size-4 rounded-full" />
+      </Button>
+    </div>
+  );
 }
