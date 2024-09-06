f50f64c739f5313107786d8953b392542dc038cf
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-06 19:29:29 +0100

    add light mode styles to title bar (#21)

--- src/components/ui/title-bar.tsx
+++ src/components/ui/title-bar.tsx
@@ -10,7 +10,7 @@ export default function TitleBar({ title }: { title: string }) {
     router.back();
   }
   return (
-    <div className="fixed flex items-center space-x-2 min-w-full fixed bg-black py-2">
+    <div className="fixed flex items-center space-x-2 min-w-full fixed dark:bg-black dark:text-white light:bg-white light:text-black py-2">
       <BackButton onClick={goBack} />
       <h2 className="text-semibold text-4xl">{title}</h2>
     </div>
