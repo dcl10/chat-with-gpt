9339f4f132787b63aa67759e84a831153d1fea7d
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-09 16:39:51 +0100

    Restyle settings page (#23)
    * use title bar for settings
    
    * compnent to show if something was saved
    
    * save indicator
    
    * unused imports
    
    * blue button on model selector
    
    * flowbite settings buttons
    
    * postion the saved toast
    
    * show if save was unsuccessful

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import BackButton from "@/components/ui/back-button";
 import { AppSettings } from "@/lib/types";
 import { Label, Select, Button } from "flowbite-react";
 import { MODEL_CHOICES } from "@/lib/constants";
-import { useRouter } from "next/navigation";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import TitleBar from "@/components/ui/title-bar";
+import { Toast } from "flowbite-react";
 
 function EditAPIKey({
   handleAPIKeyChange,
@@ -18,7 +19,7 @@ function EditAPIKey({
   setEditable: any;
 }) {
   return (
-    <div className="inline space-x-2">
+    <div className="inline-flex space-x-2 items-center">
       <label className="text-semibold" htmlFor="api-key">
         API key:&nbsp;
       </label>
@@ -29,18 +30,12 @@ function EditAPIKey({
         className="bg-transparent border-2 dark:border-white rounded-md"
         onChange={(event) => handleAPIKeyChange(event.target.value)}
       />
-      <button
-        className="rounded-md text-blue-200 border-blue-200 border-2 min-w-20"
-        onClick={saveSettings}
-      >
+      <Button color={"blue"} onClick={saveSettings}>
         Save
-      </button>
-      <button
-        className="rounded-md text-white border-white border-2 min-w-20"
-        onClick={setEditable}
-      >
+      </Button>
+      <Button color={"light"} onClick={setEditable}>
         Cancel
-      </button>
+      </Button>
     </div>
   );
 }
@@ -70,7 +65,7 @@ function EditModel({
           </option>
         ))}
       </Select>
-      <Button color={"blue"} onClick={saveSettings} outline>
+      <Button color={"blue"} onClick={saveSettings}>
         Save
       </Button>
     </div>
@@ -79,15 +74,30 @@ function EditModel({
 
 function APIKeySet({ onSetEditable }: { onSetEditable: any }) {
   return (
-    <div className="inline-flex space-x-2">
+    <div className="inline-flex space-x-2 items-center">
       <p>API key is set.</p>
-      <button
-        className="rounded-md text-red-400 border-red-400 border-2 min-w-20"
-        onClick={onSetEditable}
-      >
+      <Button color={"failure"} onClick={onSetEditable}>
         Edit
-      </button>
+      </Button>
     </div>
+  );
+}
+
+function Saved({ isSuccess, message }: { isSuccess: boolean; message: string; }) {
+  const successBg = "bg-green-200";
+  const failBg = "bg-red-200";
+  return (
+    <Toast className={`${isSuccess ? successBg : failBg} border-0 shadow-none fixed inset-x-auto bottom-4`}>
+      <div className="flex space-x-2 items-center justify-center">
+        {isSuccess ? (
+          <CheckIcon className="size-8 text-green-600" />
+        ) : (
+          <XMarkIcon className="size-8 text-red-600" />
+        )}
+
+        <p className="light:text-black dark:text-white">{message}</p>
+      </div>
+    </Toast>
   );
 }
 
@@ -95,11 +105,11 @@ export default function SettingsPage() {
   const [appSettings, setAppSettings] = useState<AppSettings>({
     apiKey: "",
     model: "",
-  })
-
-  const router = useRouter();
+  });
 
   const [isEditable, setIsEditable] = useState<boolean>();
+  const [showSaved, setShowSaved] = useState<boolean>(() => false);
+  const [isSaved, setIsSaved] = useState<boolean>(() => false);
 
   useEffect(() => {
     invoke<AppSettings>("get_settings").then((settings) => {
@@ -107,10 +117,6 @@ export default function SettingsPage() {
       settings.apiKey ? setIsEditable(false) : setIsEditable(true);
     });
   }, []);
-
-  function goBack() {
-    router.back();
-  }
 
   function handleAPIKeyChange(newValue: string): void {
     setAppSettings((prev) => ({ ...prev, apiKey: newValue }));
@@ -121,15 +127,18 @@ export default function SettingsPage() {
   }
 
   async function saveSettings(settings: AppSettings): Promise<void> {
-    await invoke("set_settings", { newSettings: settings });
+    setShowSaved((prev) => !prev);
+    const saved = await invoke<boolean>("set_settings", { newSettings: settings });
     setIsEditable(false);
+    setIsSaved(saved);
+    setTimeout(() => setShowSaved((prev) => !prev), 2000);
   }
 
   return (
-    <div className="sm:p-5 lg:p-20 space-y-2">
-      <BackButton onClick={goBack} />
+    <div className="sm:p-5 lg:p-20 space-y-2 relative">
+      <TitleBar title={"Settings"} />
+      <div className="pt-20"></div>
       <div className="space-y-2 flex flex-col">
-        <h1 className="text-3xl font-semibold pb-4">Settings</h1>
         {isEditable ? (
           <EditAPIKey
             handleAPIKeyChange={handleAPIKeyChange}
@@ -146,6 +155,7 @@ export default function SettingsPage() {
           selected={appSettings.model}
         />
       </div>
+      {showSaved && <Saved isSuccess={isSaved} message={isSaved ? "Saved!" : "Not saved..."}/>}
     </div>
   );
 }
